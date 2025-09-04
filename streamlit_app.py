da3382bf60e6eab49465586cf1b6ef5e022fe2f7
malkiavec <37767607+malkiavec@users.noreply.github.com>
2025-09-04 14:27:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 shift_based_prediction.py
 
-Run: streamlit run shift_based_prediction.py
+Run: streamlit run streamliy_app.py
 
 import itertools as it import math from collections import Counter from typing import List, Tuple, Dict
 
